74098db4a59f45a22df6e5739c951c113127faa6
Iuro <47984266+Ssination@users.noreply.github.com>
2019-11-26 11:58:26 +0000

    rbac implementado no backoffice

---
+++ advanced/common/modules/authent/module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace common\modules\authent;
+
+/**
+ * auth module definition class
+ */
+class module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'common\modules\authent\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
