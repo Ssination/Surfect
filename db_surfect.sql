929db180448c84373cfeff9b8ac718cf68225ff4
Iuro <47984266+Ssination@users.noreply.github.com>
2019-12-06 15:42:00 +0000

    Update db_surfect.sql

--- db_surfect.sql
+++ db_surfect.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 19-Nov-2019 às 13:36
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 7.2.16
+-- Tempo de geração: 06-Dez-2019 às 16:40
+-- Versão do servidor: 10.4.10-MariaDB
+-- versão do PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_surfect`
+-- Banco de dados: `db_surfect`
 --
 
 -- --------------------------------------------------------
@@ -56,6 +56,98 @@ CREATE TABLE `adresses` (
 
 INSERT INTO `adresses` (`adress_id`, `adress_name`, `city`, `zip_code`, `district`, `email`, `country_id`) VALUES
 (1, 'rua tal', 'alfeizerao', '0000-00-00', 'alfeizerao', 'ruifmiguel@hotmail.com', 119);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_assignment`
+--
+
+CREATE TABLE `auth_assignment` (
+  `item_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `auth_assignment`
+--
+
+INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
+('admin', 'miguelmendonca@hotmail.com', NULL),
+('admin', 'ruifmiguel@hotmail.com', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_item`
+--
+
+CREATE TABLE `auth_item` (
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `type` smallint(6) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `rule_name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data` blob DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `auth_item`
+--
+
+INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
+('admin', 1, 'admin has all options', NULL, NULL, NULL, NULL),
+('costumer', 1, 'costumer has some options', NULL, NULL, NULL, NULL),
+('create.category', 1, 'allows to create a category', NULL, NULL, NULL, NULL),
+('create.purchase', 1, 'allows user to create a purchase', NULL, NULL, NULL, NULL),
+('create.user', 1, 'allow a user to create another user', NULL, NULL, NULL, NULL),
+('guest', 1, 'user without account/ not authenticated', NULL, NULL, NULL, NULL),
+('manage_products', 1, 'allows the creation, update or deletion of products', NULL, NULL, NULL, NULL),
+('update.user', 1, 'allows to update user info', NULL, NULL, NULL, NULL),
+('view.store', 1, NULL, NULL, NULL, NULL, NULL),
+('view.user', 1, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_item_child`
+--
+
+CREATE TABLE `auth_item_child` (
+  `parent` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `child` varchar(64) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `auth_item_child`
+--
+
+INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
+('admin', 'costumer'),
+('admin', 'create.category'),
+('admin', 'create.user'),
+('admin', 'manage_products'),
+('admin', 'view.user'),
+('costumer', 'create.purchase'),
+('costumer', 'update.user'),
+('costumer', 'view.store'),
+('costumer', 'view.user'),
+('guest', 'create.user');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_rule`
+--
+
+CREATE TABLE `auth_rule` (
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `data` blob DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -267,6 +359,9 @@ CREATE TABLE `migration` (
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
 ('m000000_000000_base', 1572945901),
 ('m130524_201442_init', 1572945903),
+('m140506_102106_rbac_init', 1574699193),
+('m170907_052038_rbac_add_index_on_auth_assignment_user_id', 1574699194),
+('m180523_151638_rbac_updates_indexes_without_prefix', 1574699195),
 ('m190124_110200_add_verification_token_column_to_user_table', 1572945904);
 
 -- --------------------------------------------------------
@@ -369,7 +464,7 @@ CREATE TABLE `user` (
   `password_hash` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `password_reset_token` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(50) NOT NULL,
-  `status` smallint(6) NOT NULL DEFAULT '10',
+  `status` smallint(6) NOT NULL DEFAULT 10,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   `verification_token` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -388,22 +483,30 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`, `name`, `surname`, `birth_date`, `gender`, `phone_number`, `height`, `weight`, `photo`) VALUES
+('3pZAD7yziSNPfthoi8rJUu5CPi82_gDS', '$2y$13$pkd7uPc1oMMXTq/caUrwHOCvFDkB.WVgpaJq2tUPG84D.z9cDJc06', NULL, 'claclo@gmail.com', 10, 0, 0, 'n_2xykFPHEh6n7aeyI3F3RosVyifbhpA_1575305283', 'fernando', 'mendonca', '1999-07-99', 'M', '914547851', NULL, NULL, NULL),
+('V5mt3svm2JBCRAfo6S-k7AFfzEeF5JAD', '$2y$13$ChRJYq8hkrKBqFQ2mSQAWuDeP5VcRXSP5pwUyRlgVIeFmCLaeB06u', NULL, 'daniel@gmail.com', 10, 0, 0, 'XjW9yr-BP0M46zOOI2QXjor4fAqBOxu8_1575316768', 'daniel', 'mendonca', '1999-07-99', 'M', '914547851', NULL, NULL, NULL),
+('JOUKlFweZNFxQmU10Be7qeAwPvZh1CH8', '$2y$13$cBMy3Ctfe04yUBs3VHSU7eNhZXorQ8GSCvt7xelf.HU93Ngh6C.US', NULL, 'ferdi@gmail.com', 10, 0, 0, '-5CXmTDGdZJzWrgjRvBX0qDiHyJadvIf_1575305251', 'fernando', 'mendonca', '1999-07-99', 'M', '914547851', NULL, NULL, NULL),
+('nB3ahUZLgpYXkRuPvb0UIF351VUgszhg', '$2y$13$fx8uatnYi7g0s0i5eaBZoeIFxds4oyj90fVGNb8edsfvZ5rHOS.dC', NULL, 'miguel@gmail.com', 10, 0, 0, '2lu480mgiy7bBu_fj2Ht2hiA_nzrtvd0_1575303749', '', '', '', 'M', '', NULL, NULL, NULL),
+('iizjG6Iq1wZMDKR_410cL0UizTZ-SRlT', '$2y$13$Tx.FGfeH.1kwThNPzYt2/.Buu2QrX2n0VXyzr1KGXHVm6vYITowvS', NULL, 'miguelm@gmail.com', 10, 0, 0, 'nT6BHYiJeZQdaNEb9LLF94mgow0dszuj_1575305178', 'miguel', 'mendonca', '1999-07-99', 'M', '914547851', NULL, NULL, NULL),
 ('Ppjx-IrQIczovvIQqm-HX3R6-fAq3k6D', '$2y$13$413lxamOVQ1J3371Wxvh4uC67ygn8gBhP5FGUuM2xHZoWrlHAVWjC', NULL, 'miguelmendonca@hotmail.com', 10, 1573548037, 1573548037, 'faD67gsRxdvo0_roUfqCAR-yiCAhWX09_1573548037', 'miguel', 'mendonça', '1900-11-21', 'M', '911-544-645', NULL, NULL, ''),
+('ip5LeSf4qR088TQY8um7coHL-rAxvwDN', '$2y$13$8jCxhnjM4Ly6IrOfeGi6iOLGj.3t.30.UU6l0akvNHbdSrClfV9lq', NULL, 'pedro@gmail.com', 10, 0, 0, 'W-GNszB-CZmNIizmvAO01j7HrrAnHH7o_1575388301', 'daniel', 'mendonca', '1999-07-99', 'M', '914547851', NULL, NULL, NULL),
 ('eLkmMEXe_giQt6TuDJoS6kmtm_l79hz0', '$2y$13$N0TOYeK96i3Q6ToKQ/Ev8ub4xHtlwozGSGwjTQWGzHJxN3Zo1D4uC', NULL, 'pombito@hotmail.com', 9, 1574166911, 1574166911, 'BQgAZ7oiUn2GKnO__ReJI8kut8Zv-VWE_1574166911', 'miguel', 'potata', '2019-10-28', 'F', '165-168-589', NULL, NULL, NULL),
-('60T1z7FJbqOqDTozTb6i7CA42ET46K5X', '$2y$13$gXivA8QPoj9nHL.wThMj0O.Gwjk4zil/7UwbPv2D5yLYnXOendzoa', NULL, 'ruifmiguel@hotmail.com', 10, 1572946478, 1572946478, 'Hg1ynpDVxQ-4Ci5wvFRYiASzDrTaVDmy_1572946478', 'Rui', 'Miguel', '1999-02-17', 'M', '911-011-894', 177, '72.00', '');
+('NgaxseaMoBu12heRmAEdspDDe1JTw3Jj', '$2y$13$Ddk3wuP.iz.LRPCg/5qTvOxVV1VcCMc9Y6i5h5k/sGVHblaVz.4Qe', NULL, 'rui@gmail.com', 10, 0, 0, '6450o65JiiqG2bwxzNKXDQrIMrTRokMY_1575304002', 'claudio', '', '', 'M', '', NULL, NULL, NULL),
+('60T1z7FJbqOqDTozTb6i7CA42ET46K5X', '$2y$13$gXivA8QPoj9nHL.wThMj0O.Gwjk4zil/7UwbPv2D5yLYnXOendzoa', NULL, 'ruifmiguel@hotmail.com', 10, 1572946478, 1572946478, 'Hg1ynpDVxQ-4Ci5wvFRYiASzDrTaVDmy_1572946478', 'Rui', 'Miguel', '1999-02-17', 'M', '911-011-894', 177, '72.00', ''),
+('GaInjkqnsCJ6tj9LILWjFwg5irfi01vM', '$2y$13$isB3jhsWxm/7FeZ5L1XS9.T8XFu0AIlz9EPRHCBM82oDTfynVHmh2', NULL, 'vitor@gmail.com', 10, 0, 0, '8fy1fdkOw4MGKGRe1TBrfVF11Dz2B9Y2_1575388395', 'daniel', 'mendonca', '1999-07-99', 'M', '914547851', NULL, NULL, NULL);
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `admin`
+-- Índices para tabela `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indexes for table `adresses`
+-- Índices para tabela `adresses`
 --
 ALTER TABLE `adresses`
   ADD PRIMARY KEY (`adress_id`),
@@ -411,45 +514,73 @@ ALTER TABLE `adresses`
   ADD KEY `country_id` (`country_id`);
 
 --
--- Indexes for table `categories`
+-- Índices para tabela `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD PRIMARY KEY (`item_name`,`user_id`),
+  ADD KEY `idx-auth_assignment-user_id` (`user_id`);
+
+--
+-- Índices para tabela `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `rule_name` (`rule_name`),
+  ADD KEY `idx-auth_item-type` (`type`);
+
+--
+-- Índices para tabela `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD PRIMARY KEY (`parent`,`child`),
+  ADD KEY `child` (`child`);
+
+--
+-- Índices para tabela `auth_rule`
+--
+ALTER TABLE `auth_rule`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Índices para tabela `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Indexes for table `countries`
+-- Índices para tabela `countries`
 --
 ALTER TABLE `countries`
   ADD PRIMARY KEY (`country_id`);
 
 --
--- Indexes for table `migration`
+-- Índices para tabela `migration`
 --
 ALTER TABLE `migration`
   ADD PRIMARY KEY (`version`);
 
 --
--- Indexes for table `payments`
+-- Índices para tabela `payments`
 --
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`payment_id`);
 
 --
--- Indexes for table `photos`
+-- Índices para tabela `photos`
 --
 ALTER TABLE `photos`
   ADD PRIMARY KEY (`photo_id`),
   ADD KEY `product_id` (`product_id`);
 
 --
--- Indexes for table `products`
+-- Índices para tabela `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`),
   ADD KEY `category_id` (`category_id`);
 
 --
--- Indexes for table `purchases`
+-- Índices para tabela `purchases`
 --
 ALTER TABLE `purchases`
   ADD PRIMARY KEY (`purchase_id`),
@@ -458,72 +589,72 @@ ALTER TABLE `purchases`
   ADD KEY `payment_id` (`payment_id`);
 
 --
--- Indexes for table `purchase_details`
+-- Índices para tabela `purchase_details`
 --
 ALTER TABLE `purchase_details`
   ADD KEY `purchase_id` (`purchase_id`),
   ADD KEY `email` (`email`);
 
 --
--- Indexes for table `user`
+-- Índices para tabela `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT de tabela `admin`
 --
 ALTER TABLE `admin`
   MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `adresses`
+-- AUTO_INCREMENT de tabela `adresses`
 --
 ALTER TABLE `adresses`
   MODIFY `adress_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `categories`
+-- AUTO_INCREMENT de tabela `categories`
 --
 ALTER TABLE `categories`
   MODIFY `category_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `countries`
+-- AUTO_INCREMENT de tabela `countries`
 --
 ALTER TABLE `countries`
   MODIFY `country_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=156;
 
 --
--- AUTO_INCREMENT for table `payments`
+-- AUTO_INCREMENT de tabela `payments`
 --
 ALTER TABLE `payments`
   MODIFY `payment_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `photos`
+-- AUTO_INCREMENT de tabela `photos`
 --
 ALTER TABLE `photos`
   MODIFY `photo_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT de tabela `products`
 --
 ALTER TABLE `products`
   MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `purchases`
+-- AUTO_INCREMENT de tabela `purchases`
 --
 ALTER TABLE `purchases`
   MODIFY `purchase_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Restrições para despejos de tabelas
 --
 
 --
@@ -532,6 +663,25 @@ ALTER TABLE `purchases`
 ALTER TABLE `adresses`
   ADD CONSTRAINT `adresses_ibfk_1` FOREIGN KEY (`email`) REFERENCES `user` (`email`),
   ADD CONSTRAINT `adresses_ibfk_2` FOREIGN KEY (`country_id`) REFERENCES `countries` (`country_id`);
+
+--
+-- Limitadores para a tabela `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `photos`
